8033ba75c7043e79ab657abd82ae3a49bc6dd2d4
Devesh Pal <newdev0@outlook.com>
2021-08-03 00:12:26 +0530

    Update play.py

--- vcbot/play.py
+++ vcbot/play.py
@@ -32,8 +32,8 @@ async def join_handler(event):
             thumb = None
         title, duration = repl.file.title, repl.duration
     # CallsClient.input_file_name = song
-     group_call = group_call_factory.get_file_group_call(song, play_on_repeat=False)
-     try:
+    group_call = group_call_factory.get_file_group_call(song, play_on_repeat=False)
+    try:
         await group_call.start(event.chat_id)
     except RuntimeError:
         return await x.edit("No voice call active !")
